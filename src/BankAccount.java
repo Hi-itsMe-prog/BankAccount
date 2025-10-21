1dbe5556b1b30f29d7408b715909e1fd7fe2aca4
Mary <Anymail@mail.com>
2025-10-21 12:33:45 +0300

    commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,60 +1,88 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 class Account {
-    private int balance = 0;
+    int balance = 0;
+    final ReentrantLock lock = new ReentrantLock();
+    final Condition condition = lock.newCondition();
 
     // Метод для пополнения баланса
-    public synchronized void add(int sum) {
-        balance += sum;
-        System.out.println("Пополнение: +" + sum + " руб. Баланс: " + balance + " руб.");
-        notifyAll(); // Уведомляем все ожидающие потоки
+    void add(int sum) {
+        lock.lock();
+        try {
+            balance += sum;
+            System.out.println("Пополнение: +" + sum + " руб. Баланс: " + balance + " руб.");
+            condition.signalAll();
+        } finally {
+            lock.unlock();
+        }
     }
 
     // Метод для снятия денег
-    public synchronized void takeMoney(int sum) {
-        balance -= sum;
-        System.out.println("Снятие: -" + sum + " руб. Баланс: " + balance + " руб.");
-    }
-
-    // Метод для ожидания пополнения до нужной суммы
-    public synchronized void waitForAmount(int targetSum) throws InterruptedException {
-        System.out.println("Ожидаем накопления " + targetSum + " руб. для снятия...");
-
-        while (balance < targetSum) {
-            wait(); // Ждем, пока баланс не достигнет целевой суммы
+    void takeMoney(int sum) {
+        lock.lock();
+        try {
+            balance -= sum;
+            System.out.println("Снятие: -" + sum + " руб. Баланс: " + balance + " руб.");
+        } finally {
+            lock.unlock();
         }
-
-        System.out.println("Целевая сумма " + targetSum + " руб. достигнута!");
     }
 
-    // Геттер для баланса
-    public synchronized int getBalance() {
-        return balance;
+    // Метод для циклического ожидания и снятия
+    void waitAndWithdraw(int targetSum, int withdrawAmount, int maxOperations) throws InterruptedException {
+        lock.lock();
+        try {
+            for (int i = 0; i < maxOperations; i++) {
+                System.out.println("[" + (i + 1) + "] Ожидаем накопления " + targetSum + " руб. для снятия...");
+
+                while (balance < targetSum) {
+                    condition.await();
+                }
+
+                System.out.println("[" + (i + 1) + "] Целевая сумма " + targetSum + " руб. достигнута!");
+
+                // Снимаем деньги
+                takeMoney(withdrawAmount);
+
+                System.out.println("[" + (i + 1) + "] Операция завершена. Баланс: " + balance + " руб.");
+                System.out.println("---");
+            }
+        } finally {
+            lock.unlock();
+        }
     }
 }
 
 // Поток для пополнения счета
 class Deposit extends Thread {
-    private Account account;
-    private Random random = new Random();
+    Account account;
+    Random random = new Random();
+    boolean running = true;
 
-    public Deposit(Account account) {
+    Deposit(Account account) {
         this.account = account;
     }
 
     @Override
     public void run() {
         try {
-            // Многократно пополняем счет случайными суммами
-            for (int i = 0; i < 10; i++) {
-                int amount = random.nextInt(100) + 1; // Случайная сумма от 1 до 100
+            int operationCount = 0;
+            while (running && operationCount < 20) { // Увеличим количество пополнений
+                int amount = random.nextInt(100) + 1;
                 account.add(amount);
-                Thread.sleep(500); // Пауза между пополнениями
+                Thread.sleep(500);
+                operationCount++;
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    void stopDeposit() {
+        running = false;
     }
 }
 
@@ -64,27 +92,33 @@ public class BankAccount {
         Account account = new Account();
         Scanner in = new Scanner(System.in);
 
-        // Ввод целевой суммы для накопления
+        // Ввод сумм
         System.out.print("Введите целевую сумму для накопления (a): ");
         int a = in.nextInt();
 
-        // Ввод суммы для снятия
         System.out.print("Введите сумму для снятия (b): ");
         int b = in.nextInt();
+
+        System.out.print("Введите количество операций снятия: ");
+        int operations = in.nextInt();
 
         // Запускаем поток для пополнения счета
         Deposit depositThread = new Deposit(account);
         depositThread.start();
 
         try {
-            // Ждем, пока на счету не будет целевой суммы a
-            account.waitForAmount(a);
+            // Запускаем циклическое ожидание и снятие
+            account.waitAndWithdraw(a, b, operations);
 
-            // Снимаем b рублей
-            account.takeMoney(b);
+            // Останавливаем пополнение после завершения всех операций
+            depositThread.stopDeposit();
+
+            // Ждем завершения потока пополнения
+            depositThread.join();
 
             // Выводим итоговый баланс
-            System.out.println("Итоговый баланс: " + account.getBalance() + " руб.");
+            System.out.println("Все операции завершены!");
+            System.out.println("Итоговый баланс: " + account.balance + " руб.");
 
         } catch (InterruptedException e) {
             e.printStackTrace();
